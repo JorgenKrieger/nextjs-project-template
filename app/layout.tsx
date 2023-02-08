21d8e71d970ce536552e77982a5b1f73e5eeee68
Jorgen Krieger <jorgen@rox.nl>
2023-02-08 07:57:45 +0000

    Added dark mode through next-themes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,18 @@
 'use client'
 
+import { ThemeProvider } from 'next-themes'
+// import ThemeSwitch from '@/components/ThemeSwitch'
 import React, { useEffect } from 'react'
 import ReactDOM from 'react-dom'
 import './globals.css'
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-	if (process.env.NODE_ENV !== 'production') {
-		useEffect(() => {
+	useEffect(() => {
+		if (process.env.NODE_ENV !== 'production') {
 			const axe = require('@axe-core/react');
 			axe(React, ReactDOM, 1000);
-		},[])
-	}
+		}
+	}, [])
 
 	return (
 		<html lang="en">
@@ -19,7 +21,12 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
 			<head />
-			<body>{children}</body>
+			<body>
+				<ThemeProvider attribute='class'>
+					{/* <ThemeSwitch /> */}
+					{children}
+				</ThemeProvider>
+			</body>
 		</html>
 	);
 }
