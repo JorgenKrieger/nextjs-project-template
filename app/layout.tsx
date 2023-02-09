e415919caa94588eeab43c8b75f9ad7213a04c6f
Jorgen Krieger <jorgen@rox.nl>
2023-02-09 01:17:00 +0100

    Updated indenting

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,9 @@ export default function RootLayout({ children }: { children: ReactNode }) {
 	return (
 		<html lang="en">
 			{/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
+				<head /> will contain the components returned by the nearest parent
+				head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
+			*/}
 			<head />
 			<body>
 				<Providers>
